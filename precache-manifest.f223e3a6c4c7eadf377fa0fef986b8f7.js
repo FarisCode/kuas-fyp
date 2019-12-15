10352026b288e8582c50fdfe1a96afa6c1d02b8e
fariscode <farizjilani@gmail.com>
2019-12-16 00:36:39 +0500

    Updates

---
+++ precache-manifest.f223e3a6c4c7eadf377fa0fef986b8f7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "23236c51a9e838940dc00f5d8d098551",
+    "url": "/kuas-fyp/static/media/logoWide.23236c51.png"
+  },
+  {
+    "revision": "713b33a9884392181133",
+    "url": "/kuas-fyp/static/css/main.3837b958.chunk.css"
+  },
+  {
+    "revision": "0925b0b991e659a271e2",
+    "url": "/kuas-fyp/static/js/runtime~main.20832ad2.js"
+  },
+  {
+    "revision": "31fa6790eae064149e3e15e57b14efaa",
+    "url": "/kuas-fyp/static/media/ku.31fa6790.jpg"
+  },
+  {
+    "revision": "40e4c71ea6350326767b",
+    "url": "/kuas-fyp/static/js/2.1ad211ed.chunk.js"
+  },
+  {
+    "revision": "e95ed184777fcf7a8b5187bb06e07615",
+    "url": "/kuas-fyp/static/media/logo.e95ed184.png"
+  },
+  {
+    "revision": "f4883f421c60a6a5d90a418b4a74b841",
+    "url": "/kuas-fyp/static/media/logoWideBlue.f4883f42.png"
+  },
+  {
+    "revision": "713b33a9884392181133",
+    "url": "/kuas-fyp/static/js/main.1c321368.chunk.js"
+  },
+  {
+    "revision": "8e7fc2829885ebc18b4af8660bb7f138",
+    "url": "/kuas-fyp/static/media/alumni.8e7fc282.svg"
+  },
+  {
+    "revision": "fdb750b0ae806c55ff272cd5f0627318",
+    "url": "/kuas-fyp/static/media/admin.fdb750b0.svg"
+  },
+  {
+    "revision": "b43e383ee3573446c9cd192535e90972",
+    "url": "/kuas-fyp/static/media/department.b43e383e.svg"
+  },
+  {
+    "revision": "537860faa4bbc82c142be60183127959",
+    "url": "/kuas-fyp/static/media/faculty.537860fa.svg"
+  },
+  {
+    "revision": "066737613376bbbd84f3751c3db7d198",
+    "url": "/kuas-fyp/static/media/logoB.06673761.png"
+  },
+  {
+    "revision": "40e4c71ea6350326767b",
+    "url": "/kuas-fyp/static/css/2.24612c6a.chunk.css"
+  },
+  {
+    "revision": "00e112275faf0719c514d7b73ce456fb",
+    "url": "/kuas-fyp/index.html"
+  }
+];
